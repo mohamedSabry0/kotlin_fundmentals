0bc3551e88830bc3aabc37d0fcec779f8ef047df
mohamedSabry0 <mohamedsabry3023@gmail.com>
2022-08-22 04:00:09 +0200

    immutability example

---
+++ Immutable/src/main/kotlin/main.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>){
+    println("hello")
+    val q = Question()
+    q.Answer = "244"
+//    q.Question = ""
+}
+
+class Question {
+    var Answer:String = ""
+    val Question:String ="what is the answer to...?"
+
+}
