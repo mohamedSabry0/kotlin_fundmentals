63ad4e3c668cad58de270069b817791794c6e4d0
mohamedSabry0 <mohamedsabry3023@gmail.com>
2022-08-22 04:05:12 +0200

    string interpolation example

--- Immutable/src/main/kotlin/main.kt
+++ Immutable/src/main/kotlin/main.kt
@@ -3,10 +3,16 @@ fun main(args: Array<String>){
     val q = Question()
     q.Answer = "244"
 //    q.Question = ""
+
+    q.display()
+    println("the answer to ${q.Question} is ${q.Answer}")
 }
 
 class Question {
     var Answer:String = ""
     val Question:String ="what is the answer to...?"
 
+    fun display(){
+        println("you said $Answer")
+    }
 }
